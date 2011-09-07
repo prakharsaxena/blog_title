498c79fc2fee200cc57bb472df17bd91480a6554
Prakhar Saxena <prakharsaxena@gmail.com>
2011-09-07 16:07:04 -0400

    Using hpricot on one line.

--- lib/blog.rb
+++ lib/blog.rb
@@ -8,8 +8,6 @@ class Blog
   end
 
   def latest_title
-    doc = Net::HTTP.get(URI.parse(@url))
-    doc = Hpricot(doc)
-    result = doc.search("/html/body/div[2]/div/div[2]/div/div/h3/").first.to_s
+    result = Hpricot(Net::HTTP.get(URI.parse(@url))).search("/html/body/div[2]/div/div[2]/div/div/h3/").first.to_s
   end
 end
