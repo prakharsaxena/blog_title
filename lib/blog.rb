63150dcb3b292e0c92626c2dddd771b8e3ee55e1
Prakhar Saxena <prakharsaxena@gmail.com>
2011-09-07 15:34:24 -0400

    Done

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,24 @@
 #
 # Create your "Blog" class in this file.
 #
+
+require 'net/http'
+require 'hpricot'
+require 'pry'
+require 'open-uri'
+
+
+
+class Blog
+  def initialize input_url = nil
+    @url = input_url
+  end
+
+  def latest_title
+    @uri = URI.parse(@url)
+    
+    doc = open(@uri) { |f| Hpricot(f) }
+    result = doc.search("/html/body/div[2]/div/div[2]/div/div/h3/").first.to_s
+    puts result
+  end
+end
